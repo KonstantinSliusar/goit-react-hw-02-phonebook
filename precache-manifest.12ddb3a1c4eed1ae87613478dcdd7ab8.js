25fd9af29243219ac86606d209ed074e65256476
Konstantin <Konstantin.sl1982@gmail.com>
2021-04-29 09:57:21 +0300

    Updates

---
+++ precache-manifest.12ddb3a1c4eed1ae87613478dcdd7ab8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f97354c337de41ba8d11a2c96573a9a0",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "ecbfe4d212c2a650eac1",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.424b5a06.chunk.css"
+  },
+  {
+    "revision": "c62685db57ad66ff68b7",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.abd674a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.abd674a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecbfe4d212c2a650eac1",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.990b6107.chunk.js"
+  },
+  {
+    "revision": "65c8a2f1bf8ffcc01bde",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.d8bf734b.js"
+  }
+]);
